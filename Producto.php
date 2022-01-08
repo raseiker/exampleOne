457f41c6a82ddf418ec6da7bd1cbb5dd2a76d8cc
iker <raseiker1@gmail.com>
2022-01-08 17:18:00 -0500

    primer commit-producto.php

---
+++ Producto.php
@@ -0,0 +1,22 @@
+<?php
+namespace Producto;
+
+class Producto{
+  private $code;
+  private $name;
+
+  function __construct($c, $n){
+    $this->code = $c;
+    $this->name = $n;
+  }
+
+  function get_code(){
+    return $this->code . '<br>';
+  }
+
+  function get_name(){
+    return $this->name . '<br>';
+  }
+
+}
+
